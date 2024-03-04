411cca758de8d98ae5be860ae6c752de24bbda2e
NaMe2te <vladyushashishkin81@gmail.com>
2024-03-04 05:08:07 +0300

    fix: downgrade data access project to .net7

--- WebService/API/Controllers/EmployeeController.cs
+++ WebService/API/Controllers/EmployeeController.cs
@@ -6,7 +6,6 @@ namespace API.Controllers;
 
 [ApiController]
 [Route("api/[controller]")]
-
 public class EmployeeController : Controller
 {
     private readonly IEmployeeService _employeeService;
@@ -22,5 +21,21 @@ public class EmployeeController : Controller
     {
         var employee = await _employeeService.Add(dto);
         return Ok(employee);
+    }
+    
+    [HttpGet]
+    [Route(nameof(GetById))]
+    public async Task<ActionResult<EmployeeDto>> GetById([FromQuery] Guid id)
+    {
+        var employee = await _employeeService.Get(e => e.Id == id);
+        return Ok(employee);
+    }
+    
+    [HttpGet]
+    [Route(nameof(GetAll))]
+    public async Task<ActionResult<IEnumerable<EmployeeDto>>> GetAll()
+    {
+        var employees = await _employeeService.GetAll();
+        return Ok(employees);
     }
 }

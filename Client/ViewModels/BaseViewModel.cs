f7fb7e484518ddc89393b678706e8168b835374e
NaMe2te <vladyushashishkin81@gmail.com>
2024-03-05 01:34:31 +0300

    feat: base view model

---
+++ Client/ViewModels/BaseViewModel.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace Client.ViewModels;
+
+internal class BaseViewModel : INotifyPropertyChanged
+{
+    public event PropertyChangedEventHandler? PropertyChanged;
+
+    protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = "")
+    {
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+    }
+
+    protected virtual bool Set<T>(ref T field, T value, [CallerMemberName] string propertyName = "")
+    {
+        if (field.Equals(value)) 
+            return false;
+
+        field = value;
+        OnPropertyChanged(propertyName); 
+
+        return true;
+    }
+}

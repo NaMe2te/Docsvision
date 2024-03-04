728e8f0634ac1aa0208a40391678e314e56f14a5
NaMe2te <vladyushashishkin81@gmail.com>
2024-03-04 16:16:34 +0300

    feat: message controller

---
+++ WebService/API/Controllers/MessageController.cs
@@ -0,0 +1,41 @@
+﻿using Application.Dtos;
+using Application.Services.Abstractions;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class MessageController : Controller
+{
+    private readonly IMessageService _messageService;
+
+    public MessageController(IMessageService messageService)
+    {
+        _messageService = messageService;
+    }
+    
+    [HttpPost]
+    [Route(nameof(Create))]
+    public async Task<ActionResult<MessageDto>> Create([FromBody] MessageDto dto)
+    {
+        var message = await _messageService.Add(dto);
+        return Ok(message);
+    }
+    
+    [HttpGet]
+    [Route(nameof(GetById))]
+    public async Task<ActionResult<MessageDto>>  GetById([FromQuery] Guid id)
+    {
+        var message = await _messageService.Get(m => m.Id == id);
+        return Ok(message);
+    }
+    
+    [HttpGet]
+    [Route(nameof(GetAll))]
+    public async Task<ActionResult<IEnumerable<EmployeeDto>>> GetAll()
+    {
+        var messages = await _messageService.GetAll();
+        return Ok(messages);
+    }
+}
